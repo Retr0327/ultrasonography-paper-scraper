9e6026b001377442687e5a8e11f19f84c80500e2
Retr0327 <48786838+Retr0327@users.noreply.github.com>
2021-10-22 23:11:45 +0800

    Update jasa_scraper.py

--- usgscraper/scraper/jasa_scraper.py
+++ usgscraper/scraper/jasa_scraper.py
@@ -8,36 +8,24 @@ from typing import Literal, Optional, Union, Any
 from usgscraper.downloader import JASADownloader
 
 
-class JASAInfo(pydantic.BaseModel):
+class JASAInfo(pydantic.BaseModel):  
     """
     The JASAInfo object keeps track of an item in inventory, including title, published date, and authors.
     """
-
+    
     title: str
-    published_date: Any
-    authors: Any
-    # href: Any
-    # abstract: Any
+    published_date: str  
+    authors: Union[list, str]  
+    href: str  
 
     @pydantic.validator("authors")
     @classmethod
-    def is_author(cls, value) -> Union[dict[str, str], str]:
+    def is_author(cls, value) -> Union[str, dict[str, str]]: 
         """The is_author method makes sure there is author value definied."""
-        authors = asyncio.run(value)
-
         def create_author_info(authors):
-            key = [f"auth-{author+1}" for author in range(len(authors))]
+            key = [f'auth-{author + 1}' for author in range(len(authors))] 
             return dict(zip(key, authors))
 
-        if isinstance(authors, str):
-            return authors
-        return create_author_info(authors)
-
-    @pydantic.validator("published_date")
-    @classmethod
-    def check_content(cls, value) -> Union[str, Literal['None']]:
-        """The check_content method makes sure there is published_date value definied"""
-        output = asyncio.run(value)
-        if output == None:
-            return "None"
-        return output
+        if isinstance(value, str):
+            return value
+        return create_author_info(value)
