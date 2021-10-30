cbd0731568802ba21685f54ade92e61bd6d16167
Retr0327 <48786838+Retr0327@users.noreply.github.com>
2021-10-30 14:57:28 +0800

    Updated jslhr_downloader.py

--- usgscraper/downloader/jslhr_downloader.py
+++ usgscraper/downloader/jslhr_downloader.py
@@ -2,7 +2,7 @@ import asyncio
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
-from aiohttp import ClientSession 
+from aiohttp import ClientSession
 from abc import ABC, abstractmethod
 from fake_useragent import UserAgent
 from typing import Union, Callable, Awaitable, Optional
@@ -18,42 +18,45 @@ class DownloadingSoupStrategy(ABC):
         """Returns a soup object"""
         pass
 
+
 class SingleSoupStrategy(DownloadingSoupStrategy):
     def create_soup(self) -> Union[BeautifulSoup, str]:
-        req = requests.get(f'https://pubs.asha.org/toc/jslhr/{self.volume}/{self.issue}', headers={ "user-agent": UserAgent().google })
+        req = requests.get(
+            f"https://pubs.asha.org/toc/jslhr/{self.volume}/{self.issue}",
+            headers={"user-agent": UserAgent().google},
+        )
         soup = BeautifulSoup(req.text, "lxml")
-        article_html = soup.find(class_='titled_issues')
+        article_html = soup.find(class_="titled_issues")
         if article_html is None:
             return "no such issue"
         return article_html
 
+
 class AllSoupStrategy(DownloadingSoupStrategy):
     def create_url_list(self, issue: int) -> str:
         """The create_url_list method create a url based on the base url
-        
+
         Args:
             issue (str): the issue of a volume
-
         Returns:
             a str
         """
-        base_url = f'https://pubs.asha.org/toc/jslhr/{self.volume}/'
+        base_url = f"https://pubs.asha.org/toc/jslhr/{self.volume}/"
         return urljoin(base_url, str(issue))
 
     async def fetch(self, url: str, session) -> Union[BeautifulSoup, str]:
         """The fetch method fetch the url and session to get the soup object
-        
+
         Args:
             url (str)
             session
-
         Returns:
             a BeautifulSoup object if a issue exists, a str otherwise.
         """
-        async with session.get(url) as response:  
+        async with session.get(url) as response:
             html_body = await response.text()
-            soup = BeautifulSoup(html_body, "lxml") 
-            article_html = soup.find(class_='titled_issues')
+            soup = BeautifulSoup(html_body, "lxml")
+            article_html = soup.find(class_="titled_issues")
             if article_html is None:
                 return "no such issue"
             return article_html
@@ -61,5 +64,5 @@ class AllSoupStrategy(DownloadingSoupStrategy):
     async def create_soup(self) -> Callable[[], Awaitable[list]]:
         url_list = list(map(self.create_url_list, range(1, 13)))
         async with ClientSession(headers={"user-agent": UserAgent().google}) as session:
-            tasks = [asyncio.create_task(self.fetch(url, session)) for url in url_list] 
-            return await asyncio.gather(*tasks) 
+            tasks = [asyncio.create_task(self.fetch(url, session)) for url in url_list]
+            return await asyncio.gather(*tasks)
