3892b4e6802c64248422ae24a032a7b4c731f4f4
Retr0327 <48786838+Retr0327@users.noreply.github.com>
2021-10-31 17:05:06 +0800

    Updated jphon_downloader.py

--- usgscraper/downloader/jphon_downloader.py
+++ usgscraper/downloader/jphon_downloader.py
@@ -1,60 +1,90 @@
 import json
+import asyncio
 import aiohttp
+import requests
 from bs4 import BeautifulSoup
-from dataclasses import dataclass
-from typing import Optional
+from abc import ABC, abstractmethod
+from fake_useragent import UserAgent
+from typing import Callable, Awaitable, Optional
 
 
-@dataclass
-class JPhonDownloader:
+HEADERS = {"user-agent": UserAgent().google}
+
+
+class DownloadingJSONStrategy(ABC):
     """
-    The JPhonJSONDownloader object downloads the JSON from Journal of Phonetics
+    The DownloadingJSONStrategy object is the abstract class for downlading classes.
     """
 
-    volume: int
-    headers: dict
-    issue: Optional[int] = None
+    def __init__(self, volume: int, issue: Optional[int] = None):
+        self.volume = volume
+        self.issue = issue
 
-    @property
-    def url(self) -> str:
-        """The url property set the url based on the volume number.
+    def create_url(self, issue: str) -> str:
+        """The create_url method creates a url based on the arugment `issue`.
+
+        Args:
+            issue (str): the issue of a volume
 
         Returns:
             a str
         """
         if self.volume >= 42:
             return f"https://www.sciencedirect.com/journal/journal-of-phonetics/vol/{self.volume}/suppl/C"
-        return f"https://www.sciencedirect.com/journal/journal-of-phonetics/vol/{self.volume}/issue/{self.issue}"
+        return f"https://www.sciencedirect.com/journal/journal-of-phonetics/vol/{self.volume}/issue/{issue}"
 
-    def find_articles(self, json_info: json) -> json:
-        """The find_articles method is a strategy that finds the artice JSON data from `json_info`.
-
+    def find_articles(self, json_data: json) -> json:
+        """The find_articles method is a strategy that finds the artice JSON data from `json_data`.
         Args:
-            json_info (json): the original JSON data
-
+            json_data (json): the original JSON data
         Returns:
             a json
         """
+
         try:
-            return json.loads(json_info)["articles"]["ihp"]["data"]["issueBody"][
+            return json.loads(json_data)["articles"]["ihp"]["data"]["issueBody"][
                 "issueSec"
             ][1]["includeItem"]
         except KeyError:
-            return json.loads(json_info)["articles"]["ihp"]["data"]["issueBody"][
+            return json.loads(json_data)["articles"]["ihp"]["data"]["issueBody"][
                 "includeItem"
             ]
 
-    async def download(self) -> json:
-        """The get_json method gets the JSON data.
+    @abstractmethod
+    def download_json(self):
+        """Returns a json object"""
+        pass
 
-        Returns:
-            a json
-        """
-        async with aiohttp.ClientSession(headers=self.headers) as session:
-            async with session.get(self.url) as response:
-                html = await response.text()
-                soup = BeautifulSoup(html, "lxml")
-                json_info = json.loads(
-                    soup.find("script", {"type": "application/json"}).text
-                )
-                return self.find_articles(json_info) 
+
+class SingleJSONStrategy(DownloadingJSONStrategy):
+    """
+    The SingleJSONStrategy object downloads one json at a time.
+    """
+
+    def download_json(self) -> list[dict[str, str]]:
+        url = self.create_url(self.issue)
+        req = requests.get(url, headers=HEADERS)
+        soup = BeautifulSoup(req.text, "lxml")
+        json_data = json.loads(soup.find("script", {"type": "application/json"}).text)
+        return self.find_articles(json_data)
+
+
+class AllJSONStrategy(DownloadingJSONStrategy):
+    """
+    The AllJSONStrategy object downloads all the json at a time.
+    """
+
+    async def fetch(self, url: str, session) -> Callable[[], Awaitable[list]]:
+        async with session.get(url) as response:
+            html_body = await response.text()
+            soup = BeautifulSoup(html_body, "lxml")
+            json_info = soup.find("script", {"type": "application/json"})
+            if not json_info:
+                return "no json data"
+            return self.find_articles(json.loads(json_info.text))
+
+    async def download_json(self) -> Callable[[], Awaitable[list]]:
+        url_list = list(map(self.create_url, range(1, 7)))
+        async with aiohttp.ClientSession(headers=HEADERS) as session:
+            tasks = [asyncio.create_task(self.fetch(url, session)) for url in url_list]
+            return await asyncio.gather(*tasks)
