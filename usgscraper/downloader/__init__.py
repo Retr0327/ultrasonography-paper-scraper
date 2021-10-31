c0a29fc71e3cdd9ced827e49fd469468d0645c45
Retr0327 <48786838+Retr0327@users.noreply.github.com>
2021-10-31 21:37:03 +0800

    Added __init__.py

---
+++ usgscraper/downloader/__init__.py
@@ -0,0 +1,17 @@
+from .jphon_downloader import (
+    DownloadingJSONStrategy,
+    SingleJSONStrategy,
+    AllJSONStrategy,
+)
+
+from .jslhr_downloader import (
+    DownloadingJSLHRSoupStrategy,
+    SingleJSLHRSoupStrategy,
+    AllJSLHRSoupStrategy,
+)
+
+from .jasa_downloader import (
+    DownloadingJASASoupStrategy,
+    SingleJASASoupStrategy,
+    AllJASASoupStrategy,
+)
